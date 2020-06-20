f8b3ad909e3d51997f5013a2665375d9873fce7c
Bobby Neal <rsneal01@gmail.com>
2020-06-20 17:32:33 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,9 +19,17 @@ class Waiter
   end
   
   def meals
-  Meal.all.select do |meal|
-    meal.waiter == self #checking for waiter now
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+    end
   end
-end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+ 
+    best_tipped_meal.customer
+  end
 
 end
